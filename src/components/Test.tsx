c84483d069c0559f74c0d8e7dc3efafb0d4298a4
Nathan Cheung <nathancheung22@gmail.coM>
2022-09-26 23:29:38 -0400

    Added source files

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface TestProps {
+  name: string
+  heightInCm: number
+}
+
+const Test = (props: TestProps) => {
+  return <div>
+    <h1>Hello Worlddfgfgfgfg</h1>
+  </div>
+}
