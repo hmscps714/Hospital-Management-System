c6ca0c9b61024140c736997f159ecf4be0e0d0bc
Nathan Cheung <nathancheung22@gmail.coM>
2022-10-27 18:42:10 -0400

    added nurse/doctor sign on and register, fixed firebase issues

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "src/config/theme";
 import NavbarHome from "src/components/NavbarHome";
+import { registerPractitioner } from "src/api/auth";
 
 export const Login = () => {
+  async function asdf() {
+    const x = await registerPractitioner("test1234@test.com", "test1234", "bob", "doctor");
+    console.log(x);
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <NavbarHome />
+      <button onClick={asdf}>click me</button>
     </ThemeProvider>
   );
 };
