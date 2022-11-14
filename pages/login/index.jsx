eae887e8e7fc531c7854a377180b84a344b505ab
Mena314 <114767413+Mena314@users.noreply.github.com>
2022-11-14 15:58:30 -0500

    Created home page
    main heading gets smaller with smaller window. images get smaller and moved to the left too. buttons get smaller if you click them

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider } from "@mui/material";
 import theme from "src/config/theme";
 import Login from "src/components/LoginComponent/Login.js";
-import { NavbarHome } from "src/components/Navbar/NavbarHome"
+import { NavbarHome } from "src/components/Navbar/NavbarHome";
 
 export const login = () => {
   return (
