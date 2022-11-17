eeac0a8d7e2c0c33eb16f4e5122f926dec6c523c
David Phan <david@Davids-MBP.phub.net.cable.rogers.com>
2022-11-17 18:57:41 -0500

    updated styling for search and add buttons

--- src/components/Tables/GlobalFilter.js
+++ src/components/Tables/GlobalFilter.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import styles2 from './filter.module.css'
+import React from "react";
+import styles2 from "./filter.module.css";
 
 export const GlobalFilter = ({ filter, setFilter }) => {
   return (
-    <div className={styles2.Searchbar}>
-      <span>
-        <input className={styles2.input} placeholder="Search" value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
-      </span>
-    </div>
-  )
-}
+    // <div className={styles2.Searchbar}>
+    <input
+      className={styles2.input}
+      placeholder="Search"
+      value={filter || ""}
+      onChange={(e) => setFilter(e.target.value)}
+    />
+    // </div>
+  );
+};
--- src/components/Tables/Table.js
+++ src/components/Tables/Table.js
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import { GlobalFilter } from "./GlobalFilter";
 import styles2 from "./filter.module.css";
 
-export const Table = ({ tableData, routePath, buttonLabel}) => {
+export const Table = ({ tableData, routePath, buttonLabel }) => {
   const router = useRouter();
 
   const columnsFromData = Object.keys(tableData[0]).map((key, id) => {
@@ -52,63 +52,68 @@ export const Table = ({ tableData, routePath, buttonLabel}) => {
 
   //table structure
   return (
-    <> 
-    <div className={styles.search}>
-      <GlobalFilter filter ={globalFilter} setFilter={setGlobalFilter} />
-      <div className={styles.Btn}>
-          {/* <a href="http://localhost:3000/forms"> */}
-            <button onClick={() => router.push("/forms")}
-            className={styles.Add}>{buttonLabel}</button>
-          {/* </a> */}
+    <>
+      <div className={styles.search}>
+        <div className={styles.BtnContainer}>
+          <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+          {/* <div className={styles.Btn}> */}
+          <button onClick={() => router.push("/forms")} className={styles.Add}>
+            {buttonLabel}
+          </button>
+          {/* </div> */}
         </div>
-    </div>
-    <table {...getTableProps} className={styles.tables}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()} className={styles.tr}>
-            {
-              //access to each column using map
-              headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()} className={styles.th}>
-                  {column.render("Header")}
-                </th>
-              ))
-            }
-            <th></th>
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {
-          //access to each row
-          page.map((row) => {
-            prepareRow(row);
-            return (
-              <tr
-                {...row.getRowProps()}
-                className={styles.tr}
-                onClick={() => router.push(routePath + row.values["uid"])}
-              >
-                {
-                  //access to individual cells in the rows
-                  row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()} className={styles.td}>
-                        {cell.render("Cell")}
-                      </td>
-                    );
-                  })
-                }
-              </tr>
-            );
-          })
-        }
-      </tbody>
-    </table>
-    <div>
-        <button className={styles.previous} onClick={() => previousPage()}>Previous</button>
-        <button className={styles.next} onClick={() => nextPage()}>Next</button>
-    </div>
+      </div>
+      <table {...getTableProps} className={styles.tables}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()} className={styles.tr}>
+              {
+                //access to each column using map
+                headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()} className={styles.th}>
+                    {column.render("Header")}
+                  </th>
+                ))
+              }
+              <th></th>
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {
+            //access to each row
+            page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr
+                  {...row.getRowProps()}
+                  className={styles.tr}
+                  onClick={() => router.push(routePath + row.values["uid"])}
+                >
+                  {
+                    //access to individual cells in the rows
+                    row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()} className={styles.td}>
+                          {cell.render("Cell")}
+                        </td>
+                      );
+                    })
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+      <div>
+        <button className={styles.previous} onClick={() => previousPage()}>
+          Previous
+        </button>
+        <button className={styles.next} onClick={() => nextPage()}>
+          Next
+        </button>
+      </div>
     </>
   );
 };
