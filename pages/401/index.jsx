e3a638533e81bfedc99fb2ed0f7eb7479610e0b8
Mena314 <114767413+Mena314@users.noreply.github.com>
2022-11-29 22:36:47 -0500

    styled 401 page
    dynamic styling to 401 page

--- pages/401/index.jsx
+++ pages/401/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "./page401.module.css";
 
 export const page401 = () => {
   const router = useRouter();
   return (
     <>
-      <h1>Status code 401: You do not have permission to view this</h1>
-      <h1>STYLE ME PLZ TY</h1>
-      <button onClick={() => router.push("/login")}>Back to login</button>
-      <button onClick={() => router.push("/demo")}>Back to demo</button>
+      <div className={styles.page401}>
+        <div className={styles.contents}>
+          <img src="401/error.png" alt="" />
+          <h1>Error: Access Denied.</h1>
+          <p>
+            You don't have permission to access this page. <br />
+            Please contact an administrator if you think this is an error.{" "}
+          </p>
+          <button onClick={() => router.push("/login")}>Back to login</button>
+          <button onClick={() => router.push("/demo")}>Back to demo</button>
+        </div>
+      </div>
     </>
   );
 };
