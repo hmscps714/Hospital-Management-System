4ac773ec1124745abbd5f8985770f709946b234e
Mena314 <114767413+Mena314@users.noreply.github.com>
2022-12-01 06:31:23 -0500

    new css
    adjusted css for landing, contact, and about pages, as requested

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -8,21 +8,23 @@ export const Landing = () => {
   return (
     <div className={styles.Landing}>
       <title>MedSuite Home</title>
-      <div className={styles.Left}>
-        <h1>
-          Making your lives <strong>better.</strong>
-        </h1>
-        <p>Join today to see what great wonders await!</p>
-        <button className={styles.btn1} onClick={() => router.push("/contact")}>
-          Contact us
-        </button>
-        <button className={styles.btn2} onClick={() => router.push("/demo")}>
-          Demo
-        </button>
-      </div>
-      <div className={styles.Right}>
-        <img src="/landing/surgery.jpg" alt="img not found" className={styles.img1} />
-        <img src="/landing/holdinghands.jpg" alt="img not found" className={styles.img2} />
+      <div className={styles.contents}>
+        <div className={styles.Left}>
+          <h1>
+            Making your lives <strong>better.</strong>
+          </h1>
+          <p>Join today to see what great wonders await!</p>
+          <button className={styles.btn1} onClick={() => router.push("/contact")}>
+            Contact us
+          </button>
+          <button className={styles.btn2} onClick={() => router.push("/demo")}>
+            Demo
+          </button>
+        </div>
+        <div className={styles.Right}>
+          <img src="/landing/surgery.jpg" alt="img not found" className={styles.img1} />
+          <img src="/landing/holdinghands.jpg" alt="img not found" className={styles.img2} />
+        </div>
       </div>
     </div>
   );
